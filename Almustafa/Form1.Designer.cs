352990975741f7a3e44c1a3cf7c6d7867dbd0c19
Sadaqat Ali <49831211+sadaqat1809@users.noreply.github.com>
2021-05-22 10:16:39 +0500

    initial project

---
+++ Almustafa/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Almustafa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.w = new System.Windows.Forms.TextBox();
+            this.k = new System.Windows.Forms.TextBox();
+            this.p = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.op = new System.Windows.Forms.TextBox();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.metroPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // w
+            // 
+            this.w.AcceptsTab = true;
+            this.w.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.w.Location = new System.Drawing.Point(564, 234);
+            this.w.Name = "w";
+            this.w.Size = new System.Drawing.Size(294, 35);
+            this.w.TabIndex = 0;
+            this.w.EnabledChanged += new System.EventHandler(this.Form1_Load);
+            this.w.KeyDown += new System.Windows.Forms.KeyEventHandler(this.w_KeyDown);
+            // 
+            // k
+            // 
+            this.k.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.k.Location = new System.Drawing.Point(564, 287);
+            this.k.Name = "k";
+            this.k.Size = new System.Drawing.Size(294, 35);
+            this.k.TabIndex = 1;
+            // 
+            // p
+            // 
+            this.p.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p.Location = new System.Drawing.Point(564, 342);
+            this.p.Name = "p";
+            this.p.Size = new System.Drawing.Size(294, 35);
+            this.p.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(367, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Weight    ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(367, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kaat Raati";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label5.Location = new System.Drawing.Point(367, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Per Toola Rate";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(656, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // op
+            // 
+            this.op.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.op.ForeColor = System.Drawing.Color.Maroon;
+            this.op.Location = new System.Drawing.Point(564, 478);
+            this.op.Name = "op";
+            this.op.Size = new System.Drawing.Size(294, 35);
+            this.op.TabIndex = 11;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.metroPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.metroPanel1.Controls.Add(this.button2);
+            this.metroPanel1.Controls.Add(this.label4);
+            this.metroPanel1.Controls.Add(this.label2);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(0, 5);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(1298, 180);
+            this.metroPanel1.TabIndex = 12;
+            this.metroPanel1.UseCustomBackColor = true;
+            this.metroPanel1.UseStyleColors = true;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(290, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(586, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Main Chowk , Sector # 3, KTS Phone #  0313-5794019";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(259, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(639, 79);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Al Mustafa Jewellers Kts";
+            // 
+            // cb
+            // 
+            this.cb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cb.Location = new System.Drawing.Point(952, 472);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(119, 50);
+            this.cb.TabIndex = 13;
+            this.cb.Text = "Clear";
+            this.cb.UseVisualStyleBackColor = true;
+            this.cb.Click += new System.EventHandler(this.cb_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label6.Location = new System.Drawing.Point(367, 478);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Total Amount";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 214);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 347);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Almustafa.Properties.Resources.button_cancel;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(1201, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(47, 34);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 749);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cb);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.op);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p);
+            this.Controls.Add(this.k);
+            this.Controls.Add(this.w);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox w;
+        private System.Windows.Forms.TextBox k;
+        private System.Windows.Forms.TextBox p;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox op;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button cb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ Almustafa/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almustafa
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+             
+            InitializeComponent();
+           // w.Focus();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (w.Text == "" || k.Text == "" || p.Text == "")
+                {
+                    MessageBox.Show("Please Fill All Fields !");
+                }
+                else
+                {
+                    double a, b = 96, c, rt, z, x, v, n, m;
+                    double d = 11.664;
+                    a = Convert.ToDouble(w.Text);
+                    //a = Convert.ToInt32(w.Text);
+                   c = Convert.ToDouble(k.Text);
+                    rt = Convert.ToDouble(p.Text);
+                    z = (a / b);
+                    x = z * c;
+                    v = x - a;
+                    n = v / d;
+                    m = n * rt;
+                    op.Text = m.ToString();
+                    w.Clear();
+                    k.Clear();
+                    p.Clear();
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Invalid Enteries !!");
+            }
+        
+
+
+        }
+            
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            k.Text = "";
+          // w.TabIndex = 0;
+            //this.ActiveControl = k;
+            k.Select();
+            k.Focus(); */
+        }
+
+        private void w_KeyDown(object sender, KeyEventArgs e)
+        { /*
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+
+            } */
+        }
+
+        private void cb_Click(object sender, EventArgs e)
+        {
+            w.Clear();
+            k.Clear();
+            p.Clear();
+            op.Clear();
+        }
+    }
+}
